ca16d9c18a2b73c358f30e7395723c0c0827820a
nicolaeziman <20334064+Zimann@users.noreply.github.com>
2023-06-27 23:41:25 +0300

    adjust styles

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -1,9 +1,23 @@
-import { component$ } from "@builder.io/qwik";
-import AuthDialog from "~/routes/auth/auth-dialog";
+import { $, component$, useStylesScoped$ } from "@builder.io/qwik";
+import styles from './auth-dialog.scss?inline';
+import { useNavigate } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const navigate = useNavigate();
+  useStylesScoped$(styles);
+
+  const logUser = $(() => {
+    navigate('/demo/flower');
+  });
+
   return (
-    // <h1>Login screen placeholder</h1>
-    <AuthDialog/>
+    // TODO remove the height after populating the dialog
+    <>
+      <div class="flex border-2 border-solid border-white rounded-2xl w-64 items-center justify-center global-color-grey h-80">
+        {/*// TODO create actual login logic - use Google Login API*/}
+        <button onClick$={logUser} class="p-4 border-2 border-solid border-white rounded-2xl">Log In</button>
+      </div>
+      <img class="m-auto mt-6" height="170" width="170" src="/assets/radio.png" alt="logo image" />
+    </>
   )
 })
