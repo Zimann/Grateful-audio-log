9120d6f1512e271981f94daf04918f8a9cb0496b
nicolaeziman <20334064+Zimann@users.noreply.github.com>
2023-06-30 19:36:43 +0300

    apply tailwind based classes to components

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,10 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
-import styles from './header.scss?inline';
+import styles from "./header.scss?inline";
 
 export default component$(() => {
   useStylesScoped$(styles);
   return (
-    <header class="w-full flex items-center justify-center">
-      <p>Action Area</p>
+    <header class="w-full h-24">
     </header>
   );
 });
--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -1,5 +1,5 @@
 import { $, component$, useStylesScoped$ } from "@builder.io/qwik";
-import styles from './auth-dialog.scss?inline';
+import styles from './auth.scss?inline';
 import { useNavigate } from "@builder.io/qwik-city";
 
 export default component$(() => {
@@ -13,13 +13,13 @@ export default component$(() => {
   return (
     // TODO remove the height after populating the dialog
     <>
-      <main>
-        <section
-          class="flex border-2 border-solid border-white rounded-2xl w-64 items-center justify-center global-color-grey h-80">
+      <main class="h-screen w-full justify-center items-center flex flex-col">
+        <div
+          class="w-64 flex border-2 border-solid border-white rounded-2xl items-center justify-center global-color-grey h-80">
           {/*// TODO create actual login logic - use Google Login API*/}
           <button onClick$={logUser} class="p-4 border-2 border-solid border-white rounded-2xl">Log In</button>
-        </section>
-        <img class="m-auto mt-6" height="170" width="170" src="/assets/radio.png" alt="radio image" />
+        </div>
+        <img class="mt-6" height="170" width="170" src="/assets/radio.png" alt="radio image" />
       </main>
     </>
   )
