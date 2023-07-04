0acfcdc85e4019ca74b88c495c043945c4fcd655
nicolaeziman <20334064+Zimann@users.noreply.github.com>
2023-07-04 23:23:17 +0300

    move recorder styles to separate component

--- src/routes/home/record-log/index.tsx
+++ src/routes/home/record-log/index.tsx
@@ -1,16 +1,14 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import MainContainer from "~/components/main-container/main-container";
 import styles from './record-log.scss?inline';
+import AudioRecorder from "~/components/audio-recorder/audio-recorder";
 
 export default component$(() => {
   useStylesScoped$(styles);
 
   return (
     <MainContainer>
-      <button class="mb-3" aria-description="Record Log button">
-        <i class="hover:cursor-pointer fa fa-microphone"></i>
-      </button>
-      <span>Record Log</span>
+      <AudioRecorder/>
     </MainContainer>
   )
 });
