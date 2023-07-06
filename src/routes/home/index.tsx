bdae99f1c67e9e006d81f0bc605aa8a4f498f3ed
nicolaeziman <20334064+Zimann@users.noreply.github.com>
2023-07-06 13:41:50 +0300

    initiate dummy http request to fetch data and log it to the client

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,8 +1,26 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import MainContainer from "~/components/main-container/main-container";
+import { routeLoader$ } from "@builder.io/qwik-city";
+
+type User = {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
+// can use the requestEvent argument to access route params for example
+export const useFakeJSON = routeLoader$(async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  return await res.json();
+});
 
 export default component$(() => {
+  const signal: Array<User> = useFakeJSON().value;
+
+  useVisibleTask$(() => {
+    console.log(signal);
+  });
   return (
     <>
       {/*TODO fill this with content on how to use the web page - create a  grid layout to fill the container*/}
